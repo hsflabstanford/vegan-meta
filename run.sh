a32f1bda52170ca015fbfafb1b9a4ac53fc26d75
setgree <setgree@gmail.com>
2024-01-04 10:50:48 -0500

    folder restructuring

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # step 0: pre-analysis plan
-Rscript -e "rmarkdown::render('./vegan-meta-pap.Rmd', output_dir = './results')"
+Rscript -e "rmarkdown::render('./scripts/vegan-meta-pap.Rmd', output_dir = './results')"
 
 # step 1: meta-analysis
-Rscript -e "rmarkdown::render('./vegan-meta.Rmd', output_dir = './results')"
+Rscript -e "rmarkdown::render('./scripts/vegan-meta.Rmd', output_dir = './results')"
