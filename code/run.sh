1cfa28d5e8236098a64eaa8754e3938e5ed9356e
setgree <setgree@gmail.com>
2024-02-20 13:28:00 -0500

    a lot of renaming and moving, and a new abstract

---
+++ code/run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -ex
+
+# step 0: pre-analysis plan
+Rscript -e "rmarkdown::render('./code/vegan-meta-pap.Rmd', output_dir = '../results')"
+
+# step 1: meta-analysis
+Rscript -e "rmarkdown::render('./code/vegan-meta.Rmd', output_dir = '../results')"
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ex
-
-# step 0: pre-analysis plan
-Rscript -e "rmarkdown::render('./scripts/vegan-meta-pap.Rmd', output_dir = './results')"
-
-# step 1: meta-analysis
-Rscript -e "rmarkdown::render('./scripts/vegan-meta.Rmd', output_dir = './results')"
