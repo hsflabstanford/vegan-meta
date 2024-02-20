b828f8a8aec0b21c14ea88d67c6a7d429a11b152
setgree <setgree@gmail.com>
2024-02-20 13:36:31 -0500

    trying to import the papaja stuff to this project

--- code/run.sh
+++ code/run.sh
@@ -6,3 +6,5 @@ Rscript -e "rmarkdown::render('./code/vegan-meta-pap.Rmd', output_dir = '../resu
 
 # step 1: meta-analysis
 Rscript -e "rmarkdown::render('./code/vegan-meta.Rmd', output_dir = '../results')"
+
+Rscript -e "quarto::quarto_render('./vegan-meta.qmd')"
