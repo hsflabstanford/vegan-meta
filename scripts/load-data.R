8712c1b0867a6115d85e75ba2527c793eb2f9996
Seth Green <setgree@gmail.com>
2024-08-13 12:18:13 -0400

    add relevant library and two new studies to main dataset)

--- scripts/load-data.R
+++ scripts/load-data.R
@@ -1,4 +1,5 @@
 #' run the following only if you've made changes to the data online
+# library(googledrive)
 # drive_auth()
 # drive_download(as_id('1mPCt7HuK7URvuWcsMQokQCOGnSold-TS0NyC1EZniJk'),
 #               path = './data/vegan-meta.csv',
