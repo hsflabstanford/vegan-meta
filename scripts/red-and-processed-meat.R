6d4d64b9014ce754d2b89a73707c84510c046291
Seth Green <setgree@gmail.com>
2024-08-15 12:29:25 -0400

    fixed the way the red meat studies were being counted

--- scripts/red-and-processed-meat.R
+++ scripts/red-and-processed-meat.R
@@ -1,8 +1,9 @@
 RPMC <- read.csv("./data/RPMC-data.csv") |>
   group_by(title) |>
+  group_by(title) |>
   mutate(unique_paper_id = cur_group_id())  |> 
   ungroup() |> 
-  group_by(unique_paper_id, intervention_condition) |> 
+  group_by(unique_paper_id, study_num_within_paper) |> 
   mutate(unique_study_id = cur_group_id()) |>
   ungroup() |>
   mutate(decade = as.factor(case_when(year >= 2000 & year <= 2009 ~ "2000s",
