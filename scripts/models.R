3709077d27e9b1a4ce10dd4be8ab0242a08c5e99
Seth Green <setgree@gmail.com>
2024-09-10 12:56:41 -0400

    ok maybe it's not so bad and I don't need to be such a baby

--- scripts/models.R
+++ scripts/models.R
@@ -4,14 +4,17 @@ model <- dat |> map_robust()
 # Nudge
 choice_model <- dat |> filter(str_detect(theory, 'choice architecture')) |> map_robust()
 
-# Norms
-norms_model <- dat |> filter(theory == 'norms') |> map_robust()
+# psychology
+psychology_model <- dat |> filter(theory == 'psychology') |> map_robust()
 
 # Persuasion Overall
 persuasion_model <- dat |> filter(theory == 'persuasion') |> map_robust()
 
-# Norms + Persuasion
-norms_persuasion_model <- dat |> filter(theory == 'norms & persuasion') |> map_robust()
+# choice and persuasion
+choice_persuasion_model <- dat |> filter(theory == 'choice architecture & persuasion') |> map_robust()
+
+# psychology + Persuasion
+psychology_persuasion_model <- dat |> filter(theory == 'persuasion & psychology') |> map_robust()
 
 # Persuasion (Health)
 health_model <- dat |> filter(str_detect(secondary_theory, 'health')) |> map_robust()
