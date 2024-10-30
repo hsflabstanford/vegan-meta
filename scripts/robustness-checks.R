2c6b8e67c95bd16523449eef32d41325d3245ff6
Seth Green <setgree@gmail.com>
2024-10-30 12:44:01 -0400

    starting robustness checks

--- scripts/robustness-checks.R
+++ scripts/robustness-checks.R
@@ -1,19 +1,5 @@
-#' run the following only if you've made changes to the data online
-# library(googledrive)
-# drive_auth()
-# drive_download(as_id('1mPCt7HuK7URvuWcsMQokQCOGnSold-TS0NyC1EZniJk'),
-#               path = './data/vegan-meta.csv',
-#               overwrite = TRUE)
-
-#' options
-options(scipen = 99)
-
-#' data
-library(dplyr, warn.conflicts = F)
-library(stringr)
-source('./functions/d-calc.R')
-source('./functions/var-d-calc.R')
-dat <- read.csv('./data/vegan-meta.csv') |>
+# load robustness dataset
+robust_dat <- read.csv('./data/robustness-data.csv') |>   
   group_by(title) |>
   mutate(unique_paper_id = cur_group_id())  |> 
   ungroup() |> 
@@ -47,4 +33,13 @@ dat <- read.csv('./data/vegan-meta.csv') |>
   select(-one_of("X")) |>   
   select(author, year, title, unique_paper_id, unique_study_id, everything())
 
-RPMC <- read.csv('./data/rpmc-data.csv')
+merged_dat <- full_join(dat, robust_dat) |> 
+  select(c(-unique_study_id, unique_paper_id)) |>
+  group_by(title) |>
+  mutate(unique_paper_id = cur_group_id())  |>
+  ungroup() |>
+  group_by(unique_paper_id, study_num_within_paper) |>
+  mutate(unique_study_id = cur_group_id()) |>
+  ungroup() |>
+  select(author, year, title, unique_paper_id, unique_study_id, everything())
+
