444d5810c4c72585010b6afc1955dd6f0b39f75c
Seth Green <setgree@gmail.com>
2024-10-31 17:42:23 -0400

    updated red meat data, and also re=entering the data cleaning part to the load data script. frankly who knows how long we'll need this...though now I really think they should be one dataset, split. but who cares, small potatoes

--- scripts/load-data.R
+++ scripts/load-data.R
@@ -47,4 +47,35 @@ dat <- read.csv('./data/vegan-meta.csv') |>
   select(-one_of("X")) |>   
   select(author, year, title, unique_paper_id, unique_study_id, everything())
 
-RPMC <- read.csv('./data/rpmc-data.csv')
+RPMC <- read.csv('./data/rpmc-data.csv') |> 
+  group_by(title) |>
+  mutate(unique_paper_id = cur_group_id())  |>
+  ungroup() |>
+  group_by(unique_paper_id, study_num_within_paper) |>
+  mutate(unique_study_id = cur_group_id()) |>
+  ungroup() |>
+  mutate(decade = as.factor(case_when(year >= 2000 & year <= 2009 ~ "2000s",
+                                      year >= 2010 & year <= 2019  ~ "2010s",
+                                      year >= 2020 ~ "2020s")),
+         pub_status = case_when(
+           venue == "advocacy org publication" ~ 'Advocacy Organization',
+           venue == "Bachelor's Thesis" ~ 'Thesis',
+           venue == "Dissertation" ~ 'Thesis',
+           venue == "Master's Thesis" ~ 'Thesis',
+           venue == "SSRN (Preprint)" ~ 'Preprint',
+           str_detect(doi_or_url, "10\\.") ~ "Journal article"),
+         total_sample = n_c_post + n_c_post,
+         d = mapply(
+           FUN = d_calc,
+           stat_type = eff_type,
+           stat =  u_s_d,
+           sample_sd = ctrl_sd,
+           n_t = n_t_post,
+           n_c = n_c_post),
+         var_d = mapply(
+           FUN = var_d_calc,
+           d = d,
+           n_c = n_c_post,
+           n_t = n_t_post),
+         se_d = sqrt(var_d)) |>
+  select(author, year, title, unique_paper_id, unique_study_id, everything())
