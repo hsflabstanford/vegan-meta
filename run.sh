8e0a6049398d99519787962d355dff984eb91f39
Seth Green <setgree@gmail.com>
2024-11-03 12:23:09 -0500

    bibtex tidy as a command line in shell as opposed to always using a website

--- run.sh
+++ run.sh
@@ -2,3 +2,7 @@
 set -ex
 
 Rscript -e "rmarkdown::render('./manuscript/MAP-reduction-meta.Rmd', clean = T)"
+
+
+# to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy`)
+# bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./manuscript/vegan-refs.bib
