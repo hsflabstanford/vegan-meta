8bfd57894f9377e2cd155340847f9b6f5070300a
Seth Green <setgree@gmail.com>
2024-11-05 09:54:09 -0500

    lots of cleaning

--- scripts/prisma-script.R
+++ scripts/prisma-script.R
@@ -1,3 +1,5 @@
+library(dplyr)
+library(stringr)
 source('./scripts/load-data.R') # if needed
 source('./scripts/robustness-checks.R')
 source('./scripts/robustness-checks.R')
@@ -11,5 +13,13 @@ all_papers <- full_join(
   RPMC |> select(author, year, title, source, inclusion_exclusion)) |>
   full_join(robust_dat |> select(author, year, title, source, inclusion_exclusion)) |>
   full_join(excluded_data |> select(author, year, title, source, inclusion_exclusion)) |>
-  group_by(title) |> slice(1)
-table(all_papers$source)
+  group_by(title) |> slice(1) |>
+  mutate(grouped_source = 
+           str_detect(source, "snowball") ~ 'snowball search',
+         str_detect(source, 'prior review') ~ 'prior review',
+         str_detect(source, 'other') ~ 'other',
+         str_detect(source, 'database search') ~ 'database search',
+         str_detect(source, 'registry') ~ 'registry',
+         str_detect(source, 'AI search tool') ~ 'AI search tool',
+         TRUE ~ "NAAAA")
+         
