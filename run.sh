431b95366362f294149e7875be5df51b5d58cac5
Seth Green <setgree@gmail.com>
2024-11-06 10:10:52 -0500

    updated vcodebooks, shell script, one fewer library, organized bibliography

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ex
 
-Rscript -e "rmarkdown::render('./manuscript/MAP-reduction-meta.Rmd', clean = T)"
+Rscript -e "rmarkdown::render('./MAP-reduction-meta.Rmd', clean = T)"
 
 
 # to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy`)
-# bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./manuscript/vegan-refs.bib
+bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./vegan-refs.bib
