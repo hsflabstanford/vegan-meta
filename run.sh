cfe1a62155de7496a280a08f0694ba883d1d4568
Seth Green <setgree@gmail.com>
2024-11-08 15:11:21 -0500

    nightmare of a time getting !ht to be the default

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@ set -ex
 
 Rscript -e "rmarkdown::render('./MAP-reduction-meta.Rmd', clean = T)"
 
+# Replace all instances of [H] with [!ht] in the generated .tex file
+sed -i '' 's/\\begin{table}\[!h\]/\\begin{table}[!ht]/g' MAP-reduction-meta.tex
+
+Rscript -e "tinytex::latexmk('MAP-reduction-meta.tex')"
+
 
 # to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy`)
 bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./vegan-refs.bib
--- scripts/table-one-meta.R
+++ scripts/table-one-meta.R
@@ -1,50 +1,38 @@
 meta_table <- bind_rows(
-  # Overall row
   run_subset_meta_analysis(data = dat, approach_name = "Overall", col_name = "Approach"),
-  
-  # Theory-specific results with proper Approach names
-  bind_rows(
-    run_subset_meta_analysis(data = dat, filter_string = "Choice Architecture", 
-                             filter_column = "theory", approach_name = "Choice Architecture", col_name = "Approach"),
+    bind_rows(
+run_subset_meta_analysis(data = dat, filter_string = "Choice Architecture", 
+                             filter_column = "theory", approach_name = "Choice Architecture",
+                         col_name = "Approach"),
     run_subset_meta_analysis(data = dat, filter_string = "Psychology", 
-                             filter_column = "theory", approach_name = "Psychology", col_name = "Approach"),
+                             filter_column = "theory", approach_name = "Psychology", 
+                             col_name = "Approach"),
     run_subset_meta_analysis(data = dat, filter_string = "Persuasion", 
                              filter_column = "theory", approach_name = "Persuasion",
                              str_detect_flag = FALSE, col_name = "Approach"),
     run_subset_meta_analysis(data = dat, filter_string = "Persuasion & Psychology", 
-                             filter_column = "theory", approach_name = "Persuasion \\& Psychology", col_name = "Approach")),
-
-  # Type of Persuasion-specific results with proper Approach names
+                             filter_column = "theory", approach_name = "Persuasion \\& Psychology", 
+                             col_name = "Approach")),
   bind_rows(
     run_subset_meta_analysis(data = dat, filter_string = "animal", 
-                             filter_column = "secondary_theory", approach_name = "Animal Welfare", col_name = "Approach"),
+                             filter_column = "secondary_theory", approach_name = "Animal Welfare", 
+                             col_name = "Approach"),
     run_subset_meta_analysis(data = dat, filter_string = "environment", 
-                             filter_column = "secondary_theory", approach_name = "Environment", col_name = "Approach"),
+                             filter_column = "secondary_theory", approach_name = "Environment", 
+                             col_name = "Approach"),
     run_subset_meta_analysis(data = dat, filter_string = "health", 
-                             filter_column = "secondary_theory", approach_name = "Health", col_name = "Approach"))) |>
-  kbl(
-    booktabs = TRUE, 
-    col.names = c("Approach", "N (Studies)", "N (Estimates)", 
+                             filter_column = "secondary_theory", approach_name = "Health", 
+                             col_name = "Approach"))) |>
+  kbl(booktabs = TRUE, 
+      col.names = c("Approach", "N (Studies)", "N (Estimates)", 
                   "SMD", "95\\% CIs", "p val"),
     caption = "Theory-based Meta-analytic Results", 
-    align = 'l',
-    label = "table_one", escape = FALSE) |>             
+    align = 'l', label = "table_one", escape = FALSE) |>             
   kable_styling(full_width = FALSE, latex_options = "hold_position") |>
-  # Group Rows
-  pack_rows(
-    group_label = "Theory", 
-    start_row = 2, 
-    end_row = 5, 
-    latex_gap_space = "0.5em", 
-    bold = TRUE) |>
-  pack_rows(
-    group_label = "Type of Persuasion", 
-    start_row = 6, 
-    end_row = 8, 
-    latex_gap_space = "0.5em", 
-    bold = TRUE) |> 
-  # Add Footnote
-  add_footnote(
-    "Types of persuasion Ns include both Persuasion and Persuasion and Psychology studies. Additionally, many such studies combine multiple categories of argument (e.g. the health and environmental benefits of reducing MAP consumption), so the Ns for these categories will not total to the N of Persuasion + Persuasion & Psychology.",
+  pack_rows(group_label = "Theory",  start_row = 2,  end_row = 5,  
+            latex_gap_space = "0.5em",bold = TRUE) |>
+  pack_rows(group_label = "Type of Persuasion",  start_row = 6, end_row = 8,  
+            latex_gap_space = "0.5em",  bold = TRUE) |> 
+  add_footnote("Types of persuasion Ns include both Persuasion and Persuasion and Psychology studies. Additionally, many such studies combine multiple categories of argument (e.g. the health and environmental benefits of reducing MAP consumption), so the Ns for these categories will not total to the N of Persuasion + Persuasion & Psychology.",
     notation = "none")
 
