05b348ccc40e4e85aa7c2c57b4aa7c0804989341
Seth Green <setgree@gmail.com>
2024-11-15 14:03:58 -0500

    file path stuff and I guess we're keeping it simple in terms of main tex file,. ,a lot of the guidelines I was reading were for Nature the *journal* not the Nature _family_ of journals

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ Rscript -e "tinytex::pdflatex('supplement-MAP-reduction.tex')"
 
 # move results to results and get rid of aux stuff
 mv *{.pdf,.tex} ./results && rm *.{aux,out,blg}
-
+mv ./figures/*.pdf ./results/figures
 # to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy`)
 
 bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./vegan-refs.bib
