138025dd9c4be0e64b4cbbe7800619c1d376dc5f
Seth Green <setgree@gmail.com>
2024-11-16 13:47:21 -0500

    revised shell script, to just do one PDF at a time; Maya's suggetions for abstract and keywords

--- run.sh
+++ run.sh
@@ -7,19 +7,20 @@ set -ex
 # ln -s ../results ./results
 # mkdir ../results/figures
 
+# knit main script & replace all instances of [H] with [!ht] in the generated .tex file
 Rscript -e "rmarkdown::render('./MAP-reduction-meta.Rmd', clean = T)"
-Rscript -e "rmarkdown::render('./supplement-MAP-reduction.Rmd', clean = T)"
-
-# Replace all instances of [H] with [!ht] in the generated .tex file
 sed -i '' 's/\\begin{table}\[!h\]/\\begin{table}[!ht]/g' MAP-reduction-meta.tex 
-sed -i '' 's/\\begin{table}\[!h\]/\\begin{table}[!ht]/g' supplement-MAP-reduction.tex 
-
 Rscript -e "tinytex::latexmk('MAP-reduction-meta.tex')"
-Rscript -e "tinytex::pdflatex('supplement-MAP-reduction.tex')"
-
-# move results to results and get rid of aux stuff
 mv *{.pdf,.tex} ./results && rm *.{aux,out,blg}
 mv ./figures/*.pdf ./results/figures
+
+# supplement
+Rscript -e "rmarkdown::render('./supplement-MAP-reduction.Rmd', clean = T)"
+sed -i '' 's/\\begin{table}\[!h\]/\\begin{table}[!ht]/g' supplement-MAP-reduction.tex 
+Rscript -e "tinytex::pdflatex('supplement-MAP-reduction.tex')"
+mv *{.pdf,.tex} ./results && rm *.{aux,out,blg}
+mv ./figures/*.pdf ./results/figures
+
 # to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy`)
 
 bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./vegan-refs.bib
