6b47ee5cffb2f01e9711bce45a404640b599916c
Seth Green <setgree@gmail.com>
2025-02-10 15:33:29 -0400

    few more small changes...I think we're good to go

--- run.sh
+++ run.sh
@@ -9,13 +9,10 @@ set -ex
 
 # knit main script & replace all instances of [H] with [!ht] in the generated .tex file
 Rscript -e "rmarkdown::render('./MAP-reduction-meta-appetite.Rmd', clean = T)"
-mv ./figures/*.pdf ./results/figures
-
-# supplement
 Rscript -e "rmarkdown::render('./supplement-MAP-reduction.Rmd', clean = T)"
 sed -i '' 's/\\begin{table}\[!h\]/\\begin{table}[!ht]/g' supplement-MAP-reduction.tex 
 Rscript -e "tinytex::pdflatex('supplement-MAP-reduction.tex')"
-mv *{.pdf,.tex.,.docx} ./results
+mv *{.pdf,.docx} ./results
 
 # to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy`)
 
