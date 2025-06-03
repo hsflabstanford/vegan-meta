e3bd6123f0ba7e019c5af9be190027769112b3db
Seth Green <setgree@gmail.com>
2025-06-03 11:47:39 -0400

    another cite and addressing point about common sense interpretations of effect sizes

--- run.sh
+++ run.sh
@@ -12,5 +12,5 @@ Rscript -e "rmarkdown::render('./MAP-reduction-meta-appetite.Rmd', output_dir = 
 Rscript -e "rmarkdown::render('./supplement-MAP-reduction.Rmd', output_dir = './results', clean = T)"
 
 # to sort bibliography (from package `bibtex2html`, via `npm install -g bibtex-tidy` -- requires Node.js)
-# bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./vegan-refs.bib
+bibtex-tidy --curly --numeric --tab --modify --align=13 --sort=key --duplicates=key --no-escape --sort-fields --no-remove-dupe-fields --enclosing-braces=title ./vegan-refs.bib
 
