7fd31af210b04179607a0b18dd75e5687ee63035
Seth Green <setgree@gmail.com>
2025-06-05 10:23:07 -0400

    from not than

--- scripts/table-two-moderator.R
+++ scripts/table-two-moderator.R
@@ -119,7 +119,7 @@ if (!word) {
     pack_rows("Region", start_row_region, end_row_region, bold = TRUE, italic = FALSE) |>
     pack_rows("Publication Decade", start_row_decade, end_row_decade, bold = TRUE, italic = FALSE) |>
     pack_rows("Method of Delivery", start_row_delivery, end_row_delivery, bold = TRUE, italic = FALSE) |>
-    add_footnote("Moderation analyses by differences in outcomes, population, region, decade of publication, and delivery method. The first $p$ value column tests the hypothesis that the subset of studies with a given characteristic is significantly different than an SMD of zero. The second compares effects within a given category to the reference category for that moderator. N/A $p$ values were not calculated due to an insufficient number of qualifying studies.", 
+    add_footnote("Moderation analyses by differences in outcomes, population, region, decade of publication, and delivery method. The first $p$ value column tests the hypothesis that the subset of studies with a given characteristic is significantly different from an SMD of zero. The second compares effects within a given category to the reference category for that moderator. N/A $p$ values were not calculated due to an insufficient number of qualifying studies.", 
                  escape = FALSE, 
                  notation = "none")
   
